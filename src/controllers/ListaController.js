a931c497d16ab8326b2d98b666e40ef5305a73bf
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-10-11 22:44:04 -0300

    1

--- src/controllers/ListaController.js
+++ src/controllers/ListaController.js
@@ -1,1 +1,18 @@
 const ListaService = require('../services/ListaService');
+
+module.exports = {
+  buscarTodos: async(req, res)=>{
+    let json = {error:'', result:[]};
+
+    let lista = await ListaService.buscarTodos();
+
+    for(let i in carros){
+      json.result.push({
+        codigo: lista[i].codigo,
+        nome: lista[i].nome,
+        ramal: lista[i].ramal
+      });
+    }
+    res.json(json);
+  }
+}
